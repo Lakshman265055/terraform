bcbb444de7d1f2ca7fdce0be59cc88b4319be1fc
Gundapu <lakshmanx.gundapu@intel.com>
2025-09-15 23:35:56 +0530

    ansible length function

--- loops/ec2.tf
+++ loops/ec2.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "terraform" {
-  count = 3
+
+  #lenth function
+  count = length(var.instance_name)
   ami                     = "ami-09c813fb71547fc4f"
   instance_type           = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_terra.id]
